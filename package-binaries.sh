c639af50a364bf8a7e4188b689d38ea714fd385b
Mark Milner <mark.milner@avalabs.org>
2022-11-03 17:56:20 -0400

    Fix packaging

---
+++ package-binaries.sh
@@ -0,0 +1,3 @@
+npx node-pre-gyp package --target_arch=x64 --target_platform=darwin --target=16.x
+npx node-pre-gyp package --target_arch=x64 --target_platform=linux --target=16.x
+npx node-pre-gyp package --target_arch=arm64 --target_platform=darwin --target=16.x
